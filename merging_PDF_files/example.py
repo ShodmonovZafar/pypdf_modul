ff85da6f0f4ef68775d110cf2fa7ce8e2c2cd3b3
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2023-05-20 23:26:20 +0500

    z

--- merging_PDF_files/example.py
+++ merging_PDF_files/example.py
@@ -13,10 +13,10 @@ def merging_pdf_files(merged_pdf_name: str, list_of_pdf_file_names: list[str]):
 
 def main():
     # merging_PDF_files\\merging_pdf_files_example\\
-    list_of_pdf_file_names = ["merging_PDF_files\merging_pdf_files_example\file1.pdf",
-                               "merging_PDF_files\merging_pdf_files_example\file2.pdf", 
-                               "merging_PDF_files\merging_pdf_files_example\file3.pdf"]
-    merged_pdf_name = "merging_PDF_files\merging_pdf_files_example\merged_pdf.pdf"
+    list_of_pdf_file_names = ["file1.pdf",
+                               "file2.pdf", 
+                                "file3.pdf"]
+    merged_pdf_name = "merged_pdf.pdf"
     merging_pdf_files(list_of_pdf_file_names, merged_pdf_name)
 
 if __name__ == "__main__":
